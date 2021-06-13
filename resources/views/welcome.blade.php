c69c9d47f4bca5eec42616e0b39c0caa09995474
Kevin Figuera <kevinalejandro3@gmail.com>
2021-06-13 19:39:06 -0400

    Añadiendo estilos con tailwind css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,35 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
 </head>
-<body class="antialiased">
-
-<form action="tags" method="post">
-    @csrf
-    @method('POST')
-    <input type="text" name="name" >
-    <button type="submit">Agregar</button>
-</form>
-<hr>
-<h4>Listado de Etiquetas</h4>
-<table>
-    @forelse($tags as $tag)
-        <tr>
-            <td>{{ $tag->name }}</td>
-            <td>
-                <form action="tags/{{ $tag->id }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Eliminar</button>
-                </form>
-            </td>
-        </tr>
-    @empty
-        <tr>
-            <td>No hay etiquetas</td>
-        </tr>
-    @endforelse
-</table>
+<body class="bg-gray-300 py-10">
+<div class="max-w-lg bg-white mx-auto rounded-xl p-4 shadow">
+    <form action="tags" method="post" class="flex mb-4">
+        @csrf
+        @method('POST')
+        <input type="text" name="name" class="rounded-l-lg bg-gray-200 w-full px-4 py-2 outline-none" placeholder="Agregar etiqueta">
+        <button type="submit" class="rounded-r-lg bg-blue-500 text-white px-4 py-2 outline-none ">Agregar</button>
+    </form>
+    <h4 class="text-lg font-bold text-center mb-4">Listado de Etiquetas</h4>
+    <table>
+        @forelse($tags as $tag)
+            <tr>
+                <td class="w-full px-4 py-2 border">{{ $tag->name }}</td>
+                <td>
+                    <form action="tags/{{ $tag->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="bg-red-500 text-white px-4 py-2 outline-none">Eliminar</button>
+                    </form>
+                </td>
+            </tr>
+        @empty
+            <tr>
+                <td>No hay etiquetas</td>
+            </tr>
+        @endforelse
+    </table>
+</div>
 </body>
 </html>
